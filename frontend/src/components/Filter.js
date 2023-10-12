4f11460e6b0d7334d7ecf9d1724b7f80470dbaeb
Shusa <110526280+ShukoAzusa@users.noreply.github.com>
2023-10-12 16:36:25 +1000

    Update Filter.js

--- frontend/src/components/Filter.js
+++ frontend/src/components/Filter.js
@@ -1,6 +1,7 @@
 // src/components/Filter.js
-
 import React, { useState } from 'react';
+import './Filter.css';
+import Search from './images/search icon.png'
 
 const Filter = ({ onFilterSubmit }) => {
   const [dataName, setDataName] = useState('');
@@ -11,37 +12,38 @@ const Filter = ({ onFilterSubmit }) => {
     event.preventDefault();
     setError(null); 
 
-    // 验证name
+    // 验证name 
     if (!dataName.trim()) {
         setError('Data name is required.');
         return;
-      }
+    }
 
-    // 验证Value
-    if (!/^\d+$/.test(dataValue) || parseInt(dataValue, 10) < 0) {
-        setError('Data value must be a non-negative integer.');
+    // If dataValue is provided, validate it
+    if (dataValue && (!/^\d+$/.test(dataValue) || parseInt(dataValue, 10) < 0)) {
+        setError('Data value must be a non-negative integer if provided.');
         return;
-      }
+    }
     
     onFilterSubmit(dataName, dataValue);
-
   };
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={handleSubmit} className="search-container">
       <input
         type="text"
-        placeholder="Data Name"
+        placeholder="Family/Given Name"
         value={dataName}
         onChange={(e) => setDataName(e.target.value)}
       />
       <input
         type="text"
-        placeholder="Data Value"
+        placeholder="Data Value (Optional)"
         value={dataValue}
         onChange={(e) => setDataValue(e.target.value)}
       />
-      <button type="submit">Filter</button>
+      <button type="submit" className="search-button">
+        <img src={Search} alt="Search" className="search-icon" />
+      </button>
       {error && <div className="error">{error}</div>}
     </form>
   );
